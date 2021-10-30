a22a748a5a5d9d98ff0a6c33cca64dd41477619f
aaron <aaronmlucia@gmail.com>
2021-10-29 21:05:51 -0400

    finished exercises

---
+++ app.js
@@ -0,0 +1,82 @@
+//given below:
+
+// function createInstructor(firstName, lastName){
+//     return {
+//       firstName: firstName,
+//       lastName: lastName
+//     }
+// }
+
+//ES2015 version:
+function createInstructor(firstName, lastName) {
+    return {
+        firstName,
+        lastName
+    }
+}
+
+//given:
+// var favoriteNumber = 42;
+
+// var instructor = {
+//   firstName: "Colt"
+// }
+
+// instructor[favoriteNumber] = "That is my favorite!"
+
+//ES2015 version:
+let favoriteNumber = 42;
+const instructor = {
+    firstName: 'Colt',
+    [favoriteNumber]: 'That is my favorite!'
+}
+
+//given:
+// var instructor = {
+//     firstName: "Colt",
+//     sayHi: function(){
+//       return "Hi!";
+//     },
+//     sayBye: function(){
+//       return this.firstName + " says bye!";
+//     }
+// }
+
+//ES2015 version
+const instructor2 = {
+    firstName: 'Colt',
+    sayHi(){
+        return 'Hi!';
+    },
+    sayBye(){
+        return this.firstName + ' says bye!';           //question for Ryan: why does this not require a console.log statement?
+    }
+}
+
+
+// createAnimal function
+
+// Write a function which generates an animal object. The function should accepts 3 arguments:
+
+//         species: the species of animal (‘cat’, ‘dog’)
+//         verb: a string used to name a function (‘bark’, ‘bleet’)
+//         noise: a string to be printed when above function is called (‘woof’, ‘baaa’)
+
+// Use one or more of the object enhancements we’ve covered.
+
+function createAnimal(species, verb, noise) {
+    return {
+        species,
+        [verb](){
+            return noise;
+        }
+    };
+};
+
+const d = createAnimal("dog", "bark", "Woooof!")
+// {species: "dog", bark: ƒ}
+d.bark()  //"Woooof!"
+
+const s = createAnimal("sheep", "bleet", "BAAAAaaaa")
+// {species: "sheep", bleet: ƒ}
+s.bleet() //"BAAAAaaaa"
